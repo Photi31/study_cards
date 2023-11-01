041130bd71982be9074c2998207dff170fea18f3
Photi31 <photiniya@gmail.com>
2023-11-01 16:37:04 +0300

    Change title and favicon.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
